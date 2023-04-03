3facd9ad97f308b1fb9d479650b918d691499212
Antonio-87 <davydenko.87@list.ru>
2023-04-03 20:22:08 +0300

    version - 2

--- src/js/validators.js
+++ src/js/validators.js
@@ -1,3 +1,15 @@
-export function isValidInn(value) {
-  return value;
+export function isValidInn(namberCard) {
+  let sum = 0;
+  const num = String(namberCard).replace(/\D/g, "");
+  const isOdd = num.length % 2 !== 0;
+
+  if ("" === num) return false;
+
+  for (let i = 0; i < num.length; i++) {
+    let n = parseInt(num[i], 10);
+
+    sum += (isOdd | 0) === i % 2 && 9 < (n *= 2) ? n - 9 : n;
+  }
+
+  return 0 === sum % 10;
 }
--- src/js/widget.js
+++ src/js/widget.js
@@ -5,6 +5,7 @@ export default class InnFormWidget {
     this.parentEl = parentEl;
 
     this.onSubmit = this.onSubmit.bind(this);
+    this.onInput = this.onInput.bind(this);
   }
 
   static get markup() {
@@ -12,8 +13,9 @@ export default class InnFormWidget {
         <form id="form" class="form-inline">
             <div class="form-group">
                 <input class="input" name="card_number" type="text" placeholder="Credit card number">
-                <a class="submit btn-success">Click to Validate</a>
             </div>
+            <button class="submit btn-success">Click to Validate</button>
+            <div class="tooltip-inner">Please insert a credit card number</div>
         </form>
         `;
   }
@@ -30,14 +32,20 @@ export default class InnFormWidget {
     return ".form-inline";
   }
 
+  static get tooltipSelector() {
+    return ".tooltip-inner";
+  }
+
   bindToDOM() {
     this.parentEl.insertAdjacentHTML("beforeend", InnFormWidget.markup);
 
     this.element = this.parentEl.querySelector(InnFormWidget.formSelector);
     this.submit = this.element.querySelector(InnFormWidget.submitSelector);
     this.input = this.element.querySelector(InnFormWidget.inputSelector);
+    this.tooltip = this.element.querySelector(InnFormWidget.tooltipSelector);
 
     this.element.addEventListener("submit", this.onSubmit);
+    this.element.addEventListener("input", this.onInput);
   }
 
   onSubmit(e) {
@@ -45,12 +53,23 @@ export default class InnFormWidget {
 
     const value = this.input.value;
 
-    if (isValidInn(value)) {
+    if (isValidInn(value) === true) {
       this.input.classList.add("valid");
       this.input.classList.remove("invalid");
     } else {
       this.input.classList.add("invalid");
       this.input.classList.remove("valid");
     }
+    this.input.value = "";
+    if (this.input.classList.contains("invalid")) {
+      this.tooltip.classList.add("popup");
+    }
+  }
+
+  onInput(e) {
+    e.preventDefault();
+
+    const value = this.input.value;
+    this.tooltip.classList.remove("popup");
   }
 }
